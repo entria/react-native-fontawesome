d683d2b2fcc878bef31afae2ca24400e8f5d3aad
krister <krister@sent.at>
2018-03-05 00:57:44 +0200

    move imports inside the module

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
-import React from 'react';
-import { TextProperties } from 'react-native';
-
-declare module 'react-native-fontawesome' {
+declare module "react-native-fontawesome" {
+  import React from "react";
+  import { TextProperties } from "react-native";
   export const Icons: { [key: string]: string };
   const Icon: React.ComponentClass<TextProperties>;
   export default Icon;
